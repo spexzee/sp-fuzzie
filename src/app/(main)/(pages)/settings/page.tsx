9acbf4c658825a1f0da22aabb143e54cfc890115
spexzee <abub5979@gmail.com>
2024-05-06 13:09:51 +0530

    Improved user not found

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -11,7 +11,6 @@ const Settings = async (props: Props) => {
   if (!authUser) return null
 
   const user = await db.user.findUnique({ where: { clerkId: authUser.id } })
-  if (!user) return <div>User not found</div>;
 
   const removeProfileImage = async () => {
     'use server'
@@ -60,23 +59,29 @@ const Settings = async (props: Props) => {
       <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
         <span>Settings</span>
       </h1>
-      <div className="flex flex-col gap-10 p-6">
-        <div>
-          <h2 className="text-2xl font-bold">User Profile</h2>
-          <p className="text-base text-white/50">
-            Add or update your information
-          </p>
-        </div>
-        <ProfilePicture
-          onDelete={removeProfileImage}
-          userImage={user?.profileImage || ''}
-          onUpload={uploadProfileImage}
-        />
-        <ProfileForm
-          user={user}
-          onUpdate={updateUserInfo}
-        />
-      </div>
+      {
+        user ? (
+          <div className="flex flex-col gap-10 p-6">
+            <div>
+              <h2 className="text-2xl font-bold">User Profile</h2>
+              <p className="text-base text-white/50">
+                Add or update your information
+              </p>
+            </div>
+            <ProfilePicture
+              onDelete={removeProfileImage}
+              userImage={user?.profileImage || ''}
+              onUpload={uploadProfileImage}
+            />
+            <ProfileForm
+              user={user}
+              onUpdate={updateUserInfo}
+            />
+          </div>
+        ) : (
+          <div>User not found</div>
+        )
+      }
     </div>
   )
 }
