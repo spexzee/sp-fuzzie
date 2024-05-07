e3620fc225543d6f127ce127f6317a2ef0d2205a
spexzee <abub5979@gmail.com>
2024-05-07 17:58:26 +0530

    removed links

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -14,80 +14,80 @@ export const clients = [...new Array(10)].map((client, index) => ({
 export const products = [
   {
     title: 'Moonbeam',
-    link: 'https://gomoonbeam.com',
+    link: '',
     thumbnail: '/p1.png',
   },
   {
     title: 'Cursor',
-    link: 'https://cursor.so',
+    link: '',
     thumbnail: '/p2.png',
   },
   {
     title: 'Rogue',
-    link: 'https://userogue.com',
+    link: '',
     thumbnail: '/p3.png',
   },
 
   {
     title: 'Editorially',
-    link: 'https://editorially.org',
+    link: '',
     thumbnail: '/p4.png',
   },
   {
     title: 'Editrix AI',
-    link: 'https://editrix.ai',
+    link: '',
     thumbnail: '/p5.png',
   },
   {
     title: 'Pixel Perfect',
-    link: 'https://app.pixelperfect.quest',
+    link: '',
     thumbnail: '/p6.png',
   },
 
   {
     title: 'Algochurn',
-    link: 'https://algochurn.com',
+    link: '',
     thumbnail: '/p1.png',
   },
   {
     title: 'Aceternity UI',
-    link: 'https://ui.aceternity.com',
+    link: '',
     thumbnail: '/p2.png',
   },
   {
     title: 'Tailwind Master Kit',
-    link: 'https://tailwindmasterkit.com',
+    link: '',
     thumbnail: '/p3.png',
   },
   {
     title: 'SmartBridge',
-    link: 'https://smartbridgetech.com',
+    link: '',
     thumbnail: '/p4.png',
   },
   {
     title: 'Renderwork Studio',
-    link: 'https://renderwork.studio',
+    link: '',
     thumbnail: '/p5.png',
   },
 
   {
     title: 'Creme Digital',
-    link: 'https://cremedigital.com',
+    link: '',
     thumbnail: '/p6.png',
   },
   {
     title: 'Golden Bells Academy',
-    link: 'https://goldenbellsacademy.com',
+    link: '',
     thumbnail: '/p1.png',
   },
   {
     title: 'Invoker Labs',
-    link: 'https://invoker.lol',
+    link: '',
     thumbnail: '/p2.png',
   },
   {
     title: 'E Free Invoice',
-    link: 'https://efreeinvoice.com',
+    link: '',
     thumbnail: '/p3.png',
   },
 ]
